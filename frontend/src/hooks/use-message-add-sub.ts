fd39c271ce519076be5121ab3f136e962b08afe7
matteoo.eth <mateogalic112@gmail.com>
2024-09-25 12:39:32 +0200

    add countdown parsing

--- frontend/src/hooks/use-message-add-sub.ts
+++ frontend/src/hooks/use-message-add-sub.ts
@@ -3,6 +3,7 @@ import { trpc } from "@/trpc";
 import { useQueryClient } from "@tanstack/react-query";
 import { getQueryKey } from "@trpc/react-query";
 import useCountdown from "./use-countdown";
+import { Message } from "../../../server/routers/chat";
 
 const useMessageAddSub = (username: string) => {
   const queryClient = useQueryClient();
@@ -37,20 +38,14 @@ const useMessageAddSub = (username: string) => {
 
       // `/countdown <number> <url>` - should start a visible countdown on the other persons browser, and at the end of the countdown open the new window with the `url` specified.
       if (newMessage.text.startsWith("/countdown ")) {
-        const [, count, url] = newMessage.text.split(" ");
-        if (!count || !url) return;
-
-        const parsedCount = parseInt(count, 10);
-        if (isNaN(parsedCount)) return;
-
-        const urlRegex = /^(https?:\/\/)?([^\s$.?#].[^\s]*)$/i;
-        if (!urlRegex.test(url)) return;
-
-        setCountdown({
-          counter: parsedCount,
-          url,
-          username: newMessage.username,
-        });
+        const parsedCountdown = parseCountdown(newMessage);
+        if (parsedCountdown) {
+          setCountdown({
+            counter: parsedCountdown!.counter,
+            url: parsedCountdown!.url,
+            username: newMessage.username,
+          });
+        }
       }
 
       // - When new message arrives, it should trigger a sound notification
@@ -69,6 +64,25 @@ const useMessageAddSub = (username: string) => {
   });
 
   return { countdown };
+};
+
+const parseCountdown = (newMessage: Message) => {
+  const [, count, url] = newMessage.text.split(" ");
+  if (!count || !url) return null;
+
+  const parsedCount = parseInt(count, 10);
+  if (isNaN(parsedCount)) return null;
+
+  const urlRegex =
+    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+
+  if (!urlRegex.test(url)) return null;
+
+  return {
+    counter: parsedCount,
+    url,
+    username: newMessage.username,
+  };
 };
 
 export default useMessageAddSub;
